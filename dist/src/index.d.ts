a7eb1709a4f9e27b0444b8014941461333f12727
sz-p <sz_p@outlook.com>
2019-04-09 23:41:24 +0800

    [feat]: add index.d.ts

---
+++ dist/src/index.d.ts
@@ -0,0 +1,10 @@
+interface Ioption {
+    threshold?: number;
+    onError?: any;
+    onSucess?: any;
+    onTick?: any;
+    onCheck?: any;
+}
+declare let check: (value: any, desclarartion: any, option?: Ioption | undefined) => boolean;
+declare const checkTree: (value: any, nodeDesclarartion: object, option?: Ioption | undefined) => boolean;
+export { check as default, checkTree };
